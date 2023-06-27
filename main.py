ce0621ef4285fa4106e7fe62f40e4dfe8d185a98
Fabio Sartori <124041164+f-sartori-v@users.noreply.github.com>
2023-06-27 09:57:05 +0200

    Create main.py
    Create and execute the main.py file

---
+++ main.py
@@ -0,0 +1,8 @@
+""" This is the main file of the project. """
+class HelloWorld:
+  """ Create a Hello World class """
+    def __init__(self):
+        print("Hello, World!")
+
+# Create an instance of the class
+hello = HelloWorld()
